f23d52a34ebdbaf69f1dd9dd8dea9c24d29d8a5f
Bibek Pradhan <134042662+ADD2LIST@users.noreply.github.com>
2023-05-28 08:32:18 +0530

    Update play.py

--- play.py
+++ play.py
@@ -1,40 +1,93 @@
 import streamlit as st
 
+import math
+
+def add(num1, num2):
+
+    return num1 + num2
+
+def subtract(num1, num2):
+
+    return num1 - num2
+
+def multiply(num1, num2):
+
+    return num1 * num2
+
+def divide(num1, num2):
+
+    if num2 != 0:
+
+        return num1 / num2
+
+    else:
+
+        st.error("Error: Division by zero!")
+
+        return None
+
+def square(num):
+
+    return num ** 2
+
+def square_root(num):
+
+    if num >= 0:
+
+        return math.sqrt(num)
+
+    else:
+
+        st.error("Error: Invalid input for square root!")
+
+        return None
+
 def calculator():
 
     st.title("Simple Calculator")
 
     num1 = st.number_input("Enter the first number:")
 
-    operator = st.selectbox("Select an operator:", ("+", "-", "*", "/"))
+    operator = st.selectbox("Select an operator:", ("+", "-", "*", "/", "Square", "Square Root"))
 
-    num2 = st.number_input("Enter the second number:")
+    
 
     result = 0
 
     if operator == "+":
 
-        result = num1 + num2
+        num2 = st.number_input("Enter the second number:")
+
+        result = add(num1, num2)
 
     elif operator == "-":
 
-        result = num1 - num2
+        num2 = st.number_input("Enter the second number:")
+
+        result = subtract(num1, num2)
 
     elif operator == "*":
 
-        result = num1 * num2
+        num2 = st.number_input("Enter the second number:")
+
+        result = multiply(num1, num2)
 
     elif operator == "/":
 
-        if num2 != 0:
+        num2 = st.number_input("Enter the second number:")
 
-            result = num1 / num2
+        result = divide(num1, num2)
 
-        else:
+    elif operator == "Square":
 
-            st.error("Error: Division by zero!")
+        result = square(num1)
 
-    st.write("Result: ", result)
+    elif operator == "Square Root":
+
+        result = square_root(num1)
+
+    if result is not None:
+
+        st.write("Result: ", result)
 
 calculator()
-
